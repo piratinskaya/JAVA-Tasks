3cf65818af6f6f1cc5879aa7ea71d9fee0645797
piratinskaya <50095678+piratinskaya@users.noreply.github.com>
2020-04-15 05:02:46 +0400

    Update java111.java

--- java111.java
+++ java111.java
@@ -1,4 +1,9 @@
- import java.util.Scanner;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
 
 public class Сonditional {
     public static void first() {
@@ -40,7 +45,7 @@ public class Сonditional {
         }
     }
 
-    public static void thred(){
+    public static void third(){
         Scanner in = new Scanner(System.in);
         System.out.println("Enter point A: ");
 
@@ -118,17 +123,180 @@ public class Сonditional {
             System.out.println("Winter");
         }
 
+    }
 
+    public static void array1() {
+        System.out.println("task1");
+        List<Integer> numList = inputNumbers();
+        Integer[] evenList = numList.stream().filter(i -> i % 2 == 0).toArray(Integer[]::new);
+        Integer[] oddList = numList.stream().filter(i -> i % 2 == 1).toArray(Integer[]::new);
+
+        List<Integer> resList = Stream.concat(Stream.of(evenList), Stream.of(oddList)).collect(Collectors.toList());
+
+        System.out.println(resList);
+        System.out.println("--------------------------");
+    }
+
+    public void array2() {
+        System.out.println("task2");
+        List<Integer> numList = inputNumbers();
+
+        List<Integer> resList = numList.stream()
+                .filter(i -> i % 3 == 0 || i % 9 == 0).collect(Collectors.toList());
+
+        System.out.println(resList);
+        System.out.println("--------------------------");
+    }
+
+    public void array3() {
+        System.out.println("task3");
+        List<Integer> numList = inputNumbers();
+
+        List<Integer> resList = numList.stream()
+                .filter(i -> i % 5 == 0 || i % 10 == 0).collect(Collectors.toList());
+
+        System.out.println(resList);
+    }
+
+    public void array4() {
+        System.out.println("task4");
+        List<Integer> numList = inputNumbers();
+
+        int gcd = findGCD(numList);
+        int lcm = fingLCM(numList);
+
+        System.out.println("gcd: " + gcd);
+        System.out.println("lcm: " + lcm);
+        System.out.println("--------------------------");
+    }
+
+    private static List<Integer> inputNumbers() {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+
+        List<Integer> numList = new ArrayList<>();
+
+        for (int i = 0; i < n; i++) {
+            numList.add(scanner.nextInt());
+        }
+        return numList;
+    }
+
+    private int gcd(int a, int b) {
+        while (b > 0) {
+            int temp = b;
+            b = a % b; // % is remainder
+            a = temp;
+        }
+        return a;
+    }
+
+    private int findGCD(List<Integer> list) {
+        int result = list.get(0);
+        for (int i = 1; i < list.size(); i++) {
+            result = gcd(list.get(i), result);
+
+            if (result == 1) {
+                return 1;
+            }
+        }
+
+        return result;
+    }
+
+    private int lcm(int a, int b) {
+        return a * (b / gcd(a, b));
+    }
+
+    private int fingLCM(List<Integer> list) {
+        int result = list.get(0);
+        for (int i = 1; i < list.size(); i++) {
+            result = lcm(result, list.get(i));
+        }
+        return result;
+    }
+
+    public void array5() {
+        System.out.println("task5");
+        List<Integer> numList = inputNumbers();
+
+        List<Integer> resList = numList.stream()
+                .filter(this::isPrime)
+                .collect(Collectors.toList());
+
+        System.out.println(resList);
+        System.out.println("--------------------------");
+    }
+
+    private boolean isPrime(int n) {
+        return IntStream.rangeClosed(2, (int) Math.sqrt(n))
+                .allMatch(i -> n % i != 0);
+    }
+
+    public void array6() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("task6");
+        String numString = scanner.nextLine().trim();
+        int length = numString.length();
+
+        if (length == 1) {
+            System.out.println("YES");
+            return;
+        }
+
+        int num1, num2;
+        if (length % 2 == 0) {
+            num1 = Integer.parseInt(numString.substring(0, length / 2));
+            num2 = Integer.parseInt(numString.substring(length / 2, length));
+        } else {
+            num1 = Integer.parseInt(numString.substring(0, length / 2));
+            num2 = Integer.parseInt(numString.substring(length / 2 + 1, length));
+        }
+
+        if (getNumSum(num1) == getNumSum(num2)) {
+            System.out.println("YES");
+        } else {
+            System.out.println("NO");
+        }
+        System.out.println("--------------------------");
+    }
+
+    private int getNumSum(int n) {
+        int sum = 0;
+        for (; n != 0; n /= 10) {
+            sum += (n % 10);
+        }
+        return sum;
+    }
+
+    public static void array7() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("task7");
+
+        String[] numbers = {"ноль", "один", "два", "три", "четыре", "пять", "шесть", "семь", "восемь", "девять"};
+
+        int n = scanner.nextInt();
+
+        System.out.println(numbers[n]);
+        System.out.println("--------------------------");
     }
 
     public static void main(String[] args){
-      //  first();
-      //  second();
-      //  thred();
-      //  fourth();
-      //  fives();
-      //  six();
+        //  first();
+        //  second();
+        //  third();
+        //  fourth();
+        //  fives();
+        //  six();
+
+        //array1();
+        //array2();
+        //array3();
+        //array4();
+        //array5();
+        //array6();
+        //array7();
+
     }
 
 }
-
